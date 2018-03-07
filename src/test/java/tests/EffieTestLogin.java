ea3f6b53bfc4764086077c654fa7de6e4e0bfff8
maiboroda.e <evgeniy.maiboroda@gmail.com>
2018-03-07 20:03:29 +0200

    add point method

--- src/test/java/tests/EffieTestLogin.java
+++ src/test/java/tests/EffieTestLogin.java
@@ -1,32 +1,106 @@
 package tests;
 
+import com.google.common.base.Predicate;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class EffieTestLogin {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        AppiumDriver<MobileElement> driver = null;
+//        WebDriverWait wait = new WebDriverWait(driver, 3);
+
 
         //Set the Desired Capabilities
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability("deviceName", "My Phone");
         caps.setCapability("udid", "310072c8b3d92300"); //Give Device ID of your mobile phone
+//        caps.setCapability("udid", "10.8.168.41:4444"); //Give Device wifiAsnova of your mobile phone
+//        caps.setCapability("udid", "192.168.43.40:4444"); //Give Device wifiMezu of your mobile phone
+//        caps.setCapability("udid", "10.0.3.232:4444"); //Give Device wi seleniumcamp of your mobile phone
+
+
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "5.1.1");
         caps.setCapability("appPackage", "effie.app.com.effie");
-        caps.setCapability("appActivity", "com.main.activities.LoginActivity");
-        caps.setCapability("noReset", "true");
+        caps.setCapability("appActivity", "effie.app.com.effie.main.activities.LoginActivity");
+        caps.setCapability("noReset", false);
 
         //Instantiate Appium Driver
         try {
-            AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
+            driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
 
         } catch (MalformedURLException e) {
             System.out.println(e.getMessage());
         }
+        //Added 5 seconds wait so that the app loads completely before starting with element identification
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+//        driver.manage().timeouts().implicitlyWait(60,  TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+
+
+        //Find element using ID property
+        driver.findElement(By.id("effie.app.com.effie:id/input_login_la")).clear();
+        driver.findElement(By.id("effie.app.com.effie:id/input_login_la")).sendKeys("u0mc1@ipland.com.ua");
+        driver.findElement(By.id("effie.app.com.effie:id/input_password_la")).sendKeys("testPass");
+        driver.findElement(By.id("effie.app.com.effie:id/btn_login_la")).click();
+        Thread.sleep(2000);
+//        WebElement fingerButton = wait.until(ExpectedConditions.visibilityOf(
+
+//                fingerButton.click();
+        driver.findElement(By.id("effie.app.com.effie:id/buttonStartDay")).click();
+        driver.findElement(By.id("effie.app.com.effie:id/step_button")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("effie.app.com.effie:id/sync_receive_text")).click();
+
+
+//        driver.findElement(By.id("effie.app.com.effie:id/btn_p")).click();
+        driver.findElement(By.id("android:id/button1")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("effie.app.com.effie:id/btn_p")).click();
+        Thread.sleep(20000);
+
+
+//             driver.wait(ExpectedConditions.visibilityOf(driver.findElement(By.id("wdf"))));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("effie.app.com.effie:id/btn_p"))).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.View/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.view.View/android.widget.ListView/android.widget.LinearLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.Button"))).click();
+        driver.findElement(By.id("effie.app.com.effie:id/btn_p")).click();
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.view.View/android.widget.LinearLayout[2]/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.view.View/android.view.View/android.widget.ImageButton")).click();
+        Thread.sleep(10000);
+        driver.findElement(By.id("effie.app.com.effie:id/add_point_to_route")).click();
+        driver.findElement(By.id("effie.app.com.effie:id/tt_layout_clicker")).click();
+        driver.findElement(By.id("effie.app.com.effie:id/btn_p")).click();
+
+
+
+
     }
+
+
+
+
+
 }
